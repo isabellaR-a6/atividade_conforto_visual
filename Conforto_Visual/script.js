e161cd5a08b55aa1e62901227f8d798d0b2250c6
Isabella Radael <isabella.radael09@gmail.com>
2026-02-06 18:00:49 -0300

    Add files via upload

---
+++ Conforto_Visual/script.js
@@ -0,0 +1,10 @@
+function modoDark() {
+    document.body.style.backgroundColor = "#121212";
+    document.body.style.color = "#f5f5f5";
+}
+
+function modoLight() {
+    document.body.style.backgroundColor = "antiquewhite";
+    document.body.style.color = "#000";
+}
+
